a28c5fabe54daa82d21de876ff19a1be8f975572
Gamal Al-Hetari <gn62192@gmail.com>
2017-09-05 14:58:50 +0000

    course detail & lesson

--- app/controllers/instructor/sections_controller.rb
+++ app/controllers/instructor/sections_controller.rb
@@ -15,7 +15,7 @@ class Instructor::SectionsController < ApplicationController
     
     def require_authorized_for_current_course
         if current_course.user != current_user
-            render plain: "Unauthorized", status: :unauthorized
+            render plain: 'Unauthorized', status: :unauthorized
         end
     end
     
