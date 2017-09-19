0ea2c437c0196d9b12eac8d316cec9ad370a44fd
Gamal Al-Hetari <gn62192@gmail.com>
2017-09-19 11:35:19 +0000

    new lesson form modal

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   root 'static_pages#index'
   resource :dashboard, only: [:show]
   resources :courses, only: [:index, :show] do
-    resources :enrollments, only: :create
+    resources :enrollments, only: [:create]
   end
   resources :lessons, only: [:show]
   namespace :instructor do
